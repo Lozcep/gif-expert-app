315cd5af69a07b90122ac34ca08b02e6efdb44bd
José <lozalosa@gmail.com>
2021-01-24 22:31:11 -0400

    primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const GifExpertApp = () => {
+
+    
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
+   
+   const handleAdd = () => {
+       setCategories([...categories, "HunterXHunter"])
+   }
+   
+    return (
+        <>
+          <h2>GifExpertApp</h2>
+          <hr />
+
+            <button onClick={handleAdd} >Agregar</button>
+
+          <ol>
+              {
+                  categories.map((category) => {
+                      return <li key={category} >{category} </li>
+                  })
+              }
+          </ol>
+        </>
+    )
+}
+
+export default GifExpertApp
