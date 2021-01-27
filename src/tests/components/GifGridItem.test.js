cd61a16c4065105baddb037573ca0685c44ce342
José Lozada <lozada@hostienda.com>
2021-01-27 15:58:12 -0400

    configurando los test de AddCategory y getGifs

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -7,13 +7,43 @@ describe('Pruebas en <GifGridItem> ', () => {
 
     const title = 'Un titulo'
     const url = 'http://localhost/algo.jpg'
-
+    const wrapper = shallow(<GifGridItem title={title} url={url} />)
+    
     test('debe mostrar el componente correctamente ', () => {
         
-        let wrapper = shallow(<GifGridItem title={title} url={url} />)
         expect(wrapper).toMatchSnapshot()
 
     })
+    
+    test('debe de tener un párrafo con title', () => {
+
+        const p = wrapper.find('p')
+        expect( p.text().trim() ).toBe( title )
+
+        
+    })
+
+    test('debe de tener la imagen igual al url y alt de los props', () => {
+        
+        const img = wrapper.find('img')
+
+        expect( img.prop('src') ).toBe(url)
+
+        expect(img.prop('alt') ).toBe(title)
+
+
+    })
+
+    test('debe de tener animate__fadeIn', () => {
+
+        const div = wrapper.find('div')
+
+       
+        expect(div.prop('className').includes("animate__fadeIn")).toBe(true)
+        
+    })
+    
+    
     
     
 } )
